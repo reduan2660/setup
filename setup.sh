3a99adda2c32c8a2f9644393e77ec1702d71eb57
Alve Reduan <alve.reduan.09@gmail.com>
2025-11-10 00:21:06 +0600

    activate setup

--- setup.sh
+++ setup.sh
@@ -4,12 +4,12 @@ set -e
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# echo "Running installation script..."
-# source "$SCRIPT_DIR/runners/install.sh"
-#
-# echo "Running configuration script..."
-# source "$SCRIPT_DIR/runners/config.sh"
-#
+echo "Running installation script..."
+source "$SCRIPT_DIR/runners/install.sh"
+
+echo "Running configuration script..."
+source "$SCRIPT_DIR/runners/config.sh"
+
 echo "Running shell setup script..."
 bash "$SCRIPT_DIR/scripts/setup.sh"
 
